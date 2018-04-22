dfd3a93769071292cda42031ebc445d9036db89a
goldflower <aabb0852@hotmail.com>
2018-04-22 22:00:43 +0800

    maxdoubleslicesum

--- MaxDoubleSliceSum.py
+++ MaxDoubleSliceSum.py
@@ -9,13 +9,14 @@ def solution(A):
     right_max_sum = [0] * length
     left_max_sum = [0] * length
     
-    for i in range(length-2, -1, -1):
+    for i in range(length-2, -1, -1): # right most is Z-1 < N-1
         right_max_sum[i] = max(0, right_max_sum[i+1]+A[i])
-    for i in range(1, length):
+    for i in range(1, length): # left most is 1
         left_max_sum[i] = max(0, left_max_sum[i-1]+A[i])
         
     MAX = -float('inf')
     for i in range(1, length-1):
+        # note that there is a gap between left and right 
         MAX = max(MAX, left_max_sum[i-1] + right_max_sum[i+1])
         
     return MAX

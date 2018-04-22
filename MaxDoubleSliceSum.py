c051d8d2c15e971548e50b86a223a5fa33e95bd4
goldflower <aabb0852@hotmail.com>
2018-04-22 21:59:14 +0800

    maxdoubleslicesum

---
+++ MaxDoubleSliceSum.py
@@ -0,0 +1,23 @@
+"""
+idea: 
+record max of left sub-sequence and max of right sub-sequence
+then find out the max value of the sum
+"""
+
+def solution(A):
+    length = len(A)
+    right_max_sum = [0] * length
+    left_max_sum = [0] * length
+    
+    for i in range(length-2, -1, -1):
+        right_max_sum[i] = max(0, right_max_sum[i+1]+A[i])
+    for i in range(1, length):
+        left_max_sum[i] = max(0, left_max_sum[i-1]+A[i])
+        
+    MAX = -float('inf')
+    for i in range(1, length-1):
+        MAX = max(MAX, left_max_sum[i-1] + right_max_sum[i+1])
+        
+    return MAX
+    
+    
